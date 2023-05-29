227382be522ce7f5b0abbaf25c0b0580a21f3c15
janvi kachchhi <janukachchhi2@gmail.com>
2023-05-29 06:22:39 +0530

    upadte

--- lib/screen/view/home_screen.dart
+++ lib/screen/view/home_screen.dart
@@ -22,72 +22,124 @@ class _homescreenState extends State<homescreen> {
       child: Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          backgroundColor: Colors.blue,
+          backgroundColor: Colors.black,
           title: Text(
-            "Database",
+            "DataBase",
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
           ),
         ),
-        body: Column(
-          children: [
-            TextField(
-              controller: txtcategory,
-              decoration: InputDecoration(
-                hintText: "category",
-              ),
-            ),
-            TextField(
-              controller: txtamount,
-              decoration: InputDecoration(
-                hintText: "amount",
-              ),
-            ),
-            TextField(
-              controller: txtnotes,
-              decoration: InputDecoration(
-                hintText: "notes",
-              ),
-            ),
-            TextField(
-              controller: txtdate,
-              decoration: InputDecoration(
-                hintText: "date",
-              ),
-            ),
-            TextField(
-              controller: txttime,
-              decoration: InputDecoration(
-                hintText: "time",
-              ),
-            ),
-            ElevatedButton(
-                onPressed: () async {
-                  String p1 = txtcategory.text;
-                  String p2 = txtamount.text;
-                  String p3 = txtnotes.text;
-                  String p4 = txtdate.text;
-                  String p5 = txttime.text;
-                 status = 0;
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: txtcategory,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    fillColor: Colors.black,
+                    hintText: "category",
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: txtamount,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    fillColor: Colors.black,
+                    hintText: "amount",
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: txtnotes,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    fillColor: Colors.black,
+                    hintText: "notes",
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: txtdate,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    fillColor: Colors.black,
+                    hintText: "date",
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: txttime,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    fillColor: Colors.black,
+                    hintText: "time",
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    String p1 = txtcategory.text;
+                    String p2 = txtamount.text;
+                    String p3 = txtnotes.text;
+                    String p4 = txtdate.text;
+                    String p5 = txttime.text;
+                    status = 0;
 
-                 DbHelper dbHelper = DbHelper();
-                 dbHelper.insertData(cate: txtcategory, amount: txtamount, notes: txtnotes, date: txtdate, time: txttime, status: status);
-                },
-                child: Text("income"),
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),),
-            ElevatedButton(
-                onPressed: () {
-                  String p1 = txtcategory.text;
-                  String p2 = txtamount.text;
-                  String p3 = txtnotes.text;
-                  String p4 = txtdate.text;
-                  String p5 = txttime.text;
-                  status = 1;
-                  DbHelper dbHelper = DbHelper();
-                  dbHelper.insertData(cate: txtcategory, amount: txtamount, notes: txtnotes, date: txtdate, time: txttime, status: status);
-                },
-                child: Text("expense"),
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),),
-          ],
+                    DbHelper dbHelper = DbHelper();
+                    dbHelper.insertData(
+                        cate: p1,
+                        amount: p2,
+                        notes: p3,
+                        date: p4,
+                        time: p5,
+                        status: status);
+                  },
+                  child: Text("income"),
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    String p1 = txtcategory.text;
+                    String p2 = txtamount.text;
+                    String p3 = txtnotes.text;
+                    String p4 = txtdate.text;
+                    String p5 = txttime.text;
+                    status = 1;
+                    DbHelper dbHelper = DbHelper();
+                    dbHelper.insertData(
+                        cate: p1,
+                        amount: p2,
+                        notes: p3,
+                        date: p4,
+                        time: p5,
+                        status: status);
+                  },
+                  child: Text("expense"),
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
